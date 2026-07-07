d5e75495312f5647eca8589ed9dbd7db1001f575
snowed-29 <snowed29@gmail.com>
2026-07-07 15:18:22 +0100

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const port = Number(process.env.PORT ?? 5000);
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World !");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
